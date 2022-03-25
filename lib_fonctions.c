8bcddca7f09dc5a49c80d6f684e0a1aa76bd9717
Basile De leeuw <bade-lee@fu-r7-p3.s19.be>
2022-03-25 10:43:30 +0100

    3.3

--- lib_fonctions.c
+++ lib_fonctions.c
@@ -6,7 +6,7 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 11:01:37 by bade-lee          #+#    #+#             */
-/*   Updated: 2022/03/13 11:02:20 by bade-lee         ###   ########.fr       */
+/*   Updated: 2022/03/25 10:43:23 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,28 @@ int	ft_isdigit(int c)
 	if (c >= '0' && c <= '9')
 		return (1);
 	return (0);
+}
+
+static char	*ft_strcpy(char *dest, const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s2;
+
+	s2 = malloc((ft_strlen(s1) + 1) * sizeof(char));
+	if (!s2)
+		return (0);
+	return (ft_strcpy(s2, s1));
 }
