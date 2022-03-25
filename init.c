32bc089478f0a144537b87e186b339bbe8947727
Basile De leeuw <bade-lee@fu-r7-p3.s19.be>
2022-03-25 10:41:40 +0100

    3.2

--- init.c
+++ init.c
@@ -6,13 +6,39 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 10:11:05 by bade-lee          #+#    #+#             */
-/*   Updated: 2022/03/25 10:34:16 by bade-lee         ###   ########.fr       */
+/*   Updated: 2022/03/25 10:41:34 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static char	**
+static char	**manage_param(char **argv, int argc)
+{
+	char	**infos;
+	int		i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		infos = ft_split(av[1], ' ');
+		if (!infos)
+			return (0);
+	}
+	else
+	{
+		infos = malloc(argc * sizeof(char *));
+		if (!infos)
+			return (0);
+		while (++i < argc)
+		{
+			infos[i - 1] = ft_strdup(av[i]);
+			if (!infos[i - 1])
+				free_infos(infos, 1);
+		}
+		infos[i - 1] = 0;
+	}
+	return (infos);
+}
 
 t_infos	**init_infos(char *argv, int argc)
 {
